f0eaec65b07a6948eebbdc4790a5d775122ffeb0
Alexandru Cirstea <alexandru.cirstea01@gmail.com>
2025-07-09 11:29:19 +0300

    Fix getFileEvent

--- src/main/java/com/vaultx/user/context/model/file/FileBlockchainMeta.java
+++ src/main/java/com/vaultx/user/context/model/file/FileBlockchainMeta.java
@@ -1,12 +1,16 @@
 package com.vaultx.user.context.model.file;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileBlockchainMeta {
     private UUID fileId;
     private UUID messageId;
